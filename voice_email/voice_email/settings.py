9f20b6c3bdf745c0b76fb9830f31a4121f57212c
Abhinandan Purkait <purkaitabhinandan@gmail.com>
2022-04-21 10:26:18 +0530

    Update settings.py

--- voice_email/voice_email/settings.py
+++ voice_email/voice_email/settings.py
@@ -129,5 +129,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'devil.lord.coc@gmail.com'
-EMAIL_HOST_PASSWORD = '123zdrgnko456!!DDX12'
+EMAIL_HOST_USER = '<EMAIL>'
+EMAIL_HOST_PASSWORD = '<PWD>'
